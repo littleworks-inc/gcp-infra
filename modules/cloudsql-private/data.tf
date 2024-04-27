f6ec2cc9b4d3af0aceaef7cf06ef1cfe3d880927
Amaresh Pelleti <amareswer@hotmail.com>
2024-04-27 16:00:46 -0400

    commented data source

--- modules/cloudsql-private/data.tf
+++ modules/cloudsql-private/data.tf
@@ -1,4 +1,4 @@
-data "google_compute_network" "vpc" {
-  name    = var.vpc_name
-  project = var.vpc_project_name
-}
+# data "google_compute_network" "vpc" {
+#   name    = var.vpc_name
+#   project = var.vpc_project_name
+# }
