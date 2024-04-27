a24fa2cca10c7896ab9241a5214b92f0cc5705e2
Amaresh Pelleti <amareswer@hotmail.com>
2024-04-27 16:07:34 -0400

    delete

--- main.tf
+++ main.tf
@@ -1,64 +1,64 @@
-module "enable-api" {
-  source = "./modules/enable_apis"
-  project_id = var.project_id
-  enable_api_services = ["compute.googleapis.com",
-                        "vpcaccess.googleapis.com",
-                         "pubsub.googleapis.com",
-                         "containerregistry.googleapis.com",
-                         "monitoring.googleapis.com",
-                         "iap.googleapis.com",
-                         "pubsublite.googleapis.com",
-                         "iamcredentials.googleapis.com",
-                         "serviceusage.googleapis.com",
-                         "iam.googleapis.com",
-                         "redis.googleapis.com",
-                         "sqladmin.googleapis.com",
-                         "secretmanager.googleapis.com"]
-}
+# module "enable-api" {
+#   source = "./modules/enable_apis"
+#   project_id = var.project_id
+#   enable_api_services = ["compute.googleapis.com",
+#                         "vpcaccess.googleapis.com",
+#                          "pubsub.googleapis.com",
+#                          "containerregistry.googleapis.com",
+#                          "monitoring.googleapis.com",
+#                          "iap.googleapis.com",
+#                          "pubsublite.googleapis.com",
+#                          "iamcredentials.googleapis.com",
+#                          "serviceusage.googleapis.com",
+#                          "iam.googleapis.com",
+#                          "redis.googleapis.com",
+#                          "sqladmin.googleapis.com",
+#                          "secretmanager.googleapis.com"]
+# }
 
-module "vpc_composer" {
-  source                      = "./modules/vpc_creation"
-  vpc_name                    = "test-vpc"
-  vpc_description             = "VPC network for composer deployement"
-  vpc_auto_create_subnetworks = "false"
-  vpc_routing_mode            = "REGIONAL"
-  depends_on = [ module.enable-api ]
-}
+# module "vpc_composer" {
+#   source                      = "./modules/vpc_creation"
+#   vpc_name                    = "test-vpc"
+#   vpc_description             = "VPC network for composer deployement"
+#   vpc_auto_create_subnetworks = "false"
+#   vpc_routing_mode            = "REGIONAL"
+#   depends_on = [ module.enable-api ]
+# }
 
-module "custom_subnet_01" {
-  source                          = "./modules/compute_subnetwork"
-  subnet_name                     = "snet-test-nane"
-  subnet_region                   = var.region
-  subnet_ip_cidr_range            = "10.162.0.0/20"
-  vpc_subnet_network              = module.vpc_composer.vpc_id
-  subnet_private_ip_google_access = "true"
-  vpc_secondary_subnet            = []
-}
+# module "custom_subnet_01" {
+#   source                          = "./modules/compute_subnetwork"
+#   subnet_name                     = "snet-test-nane"
+#   subnet_region                   = var.region
+#   subnet_ip_cidr_range            = "10.162.0.0/20"
+#   vpc_subnet_network              = module.vpc_composer.vpc_id
+#   subnet_private_ip_google_access = "true"
+#   vpc_secondary_subnet            = []
+# }
 
- module "storage_bucket_01"  {
-   source                = "./modules/storage"
-   region                = "northamerica-northeast1"
-   bucket_name           = "sb-dia-gcyhero-${var.label_environment}-data-staging"
-   project_name          = var.project_id
-   bucket_storage_class  = "STANDARD"
-   label_environment     = var.label_environment
-   label_application     = var.application
-}
+#  module "storage_bucket_01"  {
+#    source                = "./modules/storage"
+#    region                = "northamerica-northeast1"
+#    bucket_name           = "sb-dia-gcyhero-${var.label_environment}-data-staging"
+#    project_name          = var.project_id
+#    bucket_storage_class  = "STANDARD"
+#    label_environment     = var.label_environment
+#    label_application     = var.application
+# }
 
-module "cloudsql-sql" {
-  source            = "./modules/cloudsql-private"
-  disk_size         = "50"
-  instance_type     = "db-custom-8-30720"
-  user              = var.user
-  password          = var.password
-  vpc_project_name  = module.vpc_composer.vpc_project #var.sql_vpc_project_name
-  vpc_name          = module.vpc_composer.vpc_name  #var.sql_vpc_name
-  av_type           = "REGIONAL"    #var.sql_av_type
-  db_version        = "POSTGRES_15" #var.sql_db_version
-  db_name           = "sql-bitools-prd-02"
-  region            = "northamerica-northeast1"
-  database_project  = var.project_id
-  label_environment = var.label_environment
-  label_application = var.label_application
-  depends_on = [ module.vpc_composer ]
-}
+# module "cloudsql-sql" {
+#   source            = "./modules/cloudsql-private"
+#   disk_size         = "50"
+#   instance_type     = "db-custom-8-30720"
+#   user              = var.user
+#   password          = var.password
+#   vpc_project_name  = module.vpc_composer.vpc_project #var.sql_vpc_project_name
+#   vpc_name          = module.vpc_composer.vpc_name  #var.sql_vpc_name
+#   av_type           = "REGIONAL"    #var.sql_av_type
+#   db_version        = "POSTGRES_15" #var.sql_db_version
+#   db_name           = "sql-bitools-prd-02"
+#   region            = "northamerica-northeast1"
+#   database_project  = var.project_id
+#   label_environment = var.label_environment
+#   label_application = var.label_application
+#   depends_on = [ module.vpc_composer ]
+# }
