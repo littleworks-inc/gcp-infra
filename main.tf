fa1e33da9a97eaf9e1d9c3d4fd6441f4e8ca367f
Amaresh Pelleti <amareswer@hotmail.com>
2024-04-27 15:47:37 -0400

    updated project name

--- main.tf
+++ main.tf
@@ -32,8 +32,8 @@ module "cloudsql-sql" {
   instance_type     = "db-custom-8-30720"
   user              = var.user
   password          = var.password
-  vpc_project_name  = "lcl-net-svc" #var.sql_vpc_project_name
-  vpc_name          = "vpc-bf-pre"  #var.sql_vpc_name
+  vpc_project_name  = module.vpc_composer.vpc_project #var.sql_vpc_project_name
+  vpc_name          = module.vpc_composer.vpc_name  #var.sql_vpc_name
   av_type           = "REGIONAL"    #var.sql_av_type
   db_version        = "POSTGRES_15" #var.sql_db_version
   db_name           = "sql-bitools-prd-02"
--- modules/vpc_creation/output.tf
+++ modules/vpc_creation/output.tf
@@ -9,3 +9,11 @@ output "vpc_self_link" {
 output "vpc_gateway_ipv4" {
   value = google_compute_network.vpc.gateway_ipv4
 }
+
+output "vpc_project" {
+  value = google_compute_network.vpc.project
+}
+
+output "vpc_name" {
+  value = google_compute_network.vpc.name
+}
