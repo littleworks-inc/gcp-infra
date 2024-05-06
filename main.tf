bed58d8311e4d55b162fb880cedd935cecbdb4ca
Amaresh Pelleti <amareswer@hotmail.com>
2024-05-06 16:29:55 -0400

    private ip

--- main.tf
+++ main.tf
@@ -36,16 +36,16 @@ module "custom_subnet_01" {
   vpc_secondary_subnet            = []
 }
 
-# resource "google_compute_global_address" "private_ip_address" {
-#   # provider = google-beta
+resource "google_compute_global_address" "private_ip_address" {
+  # provider = google-beta
 
-#   name          = "private-ip-address"
-#   purpose       = "VPC_PEERING"
-#   address_type  = "INTERNAL"
-#   prefix_length = 16
-#   network       = module.vpc_creation.vpc_id
-#   depends_on    = [module.vpc_creation]
-# }
+  name          = "private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = module.vpc_creation.vpc_id
+  depends_on    = [module.vpc_creation]
+}
 
 # resource "google_service_networking_connection" "private_vpc_connection" {
 #   # provider = google-beta
